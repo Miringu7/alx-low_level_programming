6ee0851c58a4fbdbec9670def5453617c6cdbd66
miringu7 <miringusimeon@gmail.com>
2023-10-07 00:09:19 +0300

    test02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - main function of for loop
+  * Return: returns 0
+  */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a' ; lc <= 'z' ; lc++)
+	{
+		putchar(lc);
+	}
+	return (0);
+}
