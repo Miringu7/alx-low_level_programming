e9e4f35fe0784761b17e70d250ae2459631581e5
miringu7 <miringusimeon@gmail.com>
2023-10-07 01:20:05 +0300

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - main function with a for loop
+  * Return: returns 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
