cbbac7d79551ad01239f986d61b8f29fd9842fae
miringu7 <miringusimeon@gmail.com>
2023-10-07 18:11:16 +0300

    test101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+  * main - the main function writes the error message
+  * Return: returns 1
+  */
+
+int main(void)
+{
+	const char *message = "and that piece of art is useful\"\
+			- Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
+	return (1);
+}
