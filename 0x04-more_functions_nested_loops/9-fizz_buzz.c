b8df1313763ceef98f7b05d366641949937e1f30
miringu7 <miringusimeon@gmail.com>
2023-10-12 23:59:53 +0300

    fixing errors

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,23 @@ int main(void)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			printf(" FizzBuzz");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz");
-			printf(" ");
+			printf(" Fizz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz");
-			printf(" ");
+			printf(" Buzz");
+		}
+		else if (num == 1)
+		{
+			printf("%d", num);
 		}
 		else
 		{
-			printf("%d", num);
-			printf(" ");
+			printf(" %d", num);
 		}
 	}
 	printf("\n");
