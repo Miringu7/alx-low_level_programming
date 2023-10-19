f5bf4e5ac8ab2736bef5dddb7a213018b6f1423e
miringu7 <miringusimeon@gmail.com>
2023-10-19 21:04:53 +0300

    fixing errors

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 - s2 != 0)
+	if (*s1 - *s2 != 0)
 	{
-		if (s1 < s2)
+		if (*s1 < *s2)
 			/* if s1 is lesser return the difference*/
 			return (*s1 - *s2);
-		else if (s1 > s2)
+		else if (*s1 > *s2)
 			/*if s1 is greater return the difference*/
 			return (*s1 - *s2);
 	}
