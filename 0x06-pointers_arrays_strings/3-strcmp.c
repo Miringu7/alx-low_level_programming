c77d4134fa7e37472baea517157e959f64d24668
miringu7 <miringusimeon@gmail.com>
2023-10-19 21:02:34 +0300

    task3, a function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _strcmp - function that compares two strings
+  * @s1: function parameter1
+  * @s2: function parameter2
+  * Return: returns 0
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 - s2 != 0)
+	{
+		if (s1 < s2)
+			/* if s1 is lesser return the difference*/
+			return (*s1 - *s2);
+		else if (s1 > s2)
+			/*if s1 is greater return the difference*/
+			return (*s1 - *s2);
+	}
+	return (0);
+}
