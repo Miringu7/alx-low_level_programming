17625a5ec0edb563a1a3d9842ce63a7550f1f8ab
miringu7 <miringusimeon@gmail.com>
2023-10-28 00:34:58 +0300

    fixing errors

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - a program that prints the number of arguments passed into it.
+  * main - prints the number of arguments passed into it.
   * @argc: argument count
   * @argv: argument vector
   * Return: 0
