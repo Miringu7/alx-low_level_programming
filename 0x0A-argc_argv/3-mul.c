6bc2bb70c44cb5ef7db464b22ea75be2e9ace3e5
miringu7 <miringusimeon@gmail.com>
2023-10-28 17:51:42 +0300

    task3: a program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	int value1, value2;
+	int value1 = 0, value2 = 0, i1 = 0, i2 = 0, sign1 = 1, sign2 = 1;
 
 	if (argc <= 2)
 	{
@@ -18,8 +18,25 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		value1 = (*argv[1] - '0');
-		value2 = (*argv[2] - '0');
+		if (*argv[1] == '-')
+		{
+			sign1 = -1;
+			i1++;
+		}
+
+		if (*argv[2] == '-')
+		{
+			sign2 = -1;
+			i2++;
+		}
+
+		for ( ; argv[1][i1] != '\0'; ++i1)
+			value1 = value1 * 10 + (argv[1][i1] - '0');
+		value1 = value1 * sign1;
+
+		for ( ; argv[2][i2] != '\0'; ++i2)
+			value2 = value2 * 10 + (argv[2][i2] - '0');
+		value2 = value2 * sign2;
 
 		printf("%d\n", (value1 * value2));
 	}
