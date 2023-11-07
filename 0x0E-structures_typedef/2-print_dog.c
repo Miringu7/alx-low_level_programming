23764264b1f798175e2a7af6109902c4e24216e2
miringu7 <miringusimeon@gmail.com>
2023-11-07 23:23:52 +0300

    fixing errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,18 +8,13 @@
  */
 void print_dog(struct dog *d)
 {
-	d = malloc(sizeof(struct dog));
-
 	if (d == NULL)
 		return;
-
-	d->name = dog.name;
-	d->age = dog.age;
-	d->owner = dog.owner;
 
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
