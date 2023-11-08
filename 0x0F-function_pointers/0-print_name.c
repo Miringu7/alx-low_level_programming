857f85a325c3231f76af47f4231ebd3c69aa48b7
miringu7 <miringusimeon@gmail.com>
2023-11-08 21:58:38 +0300

    task0: a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: function parameter
+ * @f: function parameter
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
