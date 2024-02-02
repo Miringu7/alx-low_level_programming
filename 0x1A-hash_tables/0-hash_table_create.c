f8a2dfbda121fb741a3ec459d1d11b4aaad4d978
miringu7 <miringusimeon@gmail.com>
2024-02-02 10:22:55 +0300

    fixing errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *my_table = (hash_table_t*) calloc(sizeof(hash_node_t**), size);
+	hash_table_t *my_table = (hash_table_t *) calloc(sizeof(hash_node_t **), size);
 
 	if (!my_table)
 	{
