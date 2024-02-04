8727a0f8a967c57c1d2242cdaaf2c5cc22e4d189
miringu7 <miringusimeon@gmail.com>
2024-02-04 12:12:12 +0300

    task3 errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -58,7 +58,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	const unsigned char *u_key = (const unsigned char *)key;
 
-	if (!u_key)
+	if (!u_key || !key)
 	{
 		free(ht->array);
 		free(ht);
